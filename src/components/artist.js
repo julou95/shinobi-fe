9e88c2191e5dcce1f7abfb1478afc00e611a374a
jul <louis.jungo@ti8m.ch>
2022-11-08 19:11:22 +0100

    parallax fix

--- src/components/artist.js
+++ src/components/artist.js
@@ -7,18 +7,18 @@ const isEven = (nmbr) => nmbr % 2 == 0
 export default function Artist({ data, index }) {
   return (
     <div className={styles.artist}>
-      <div className={styles.profilePic} uk-parallax={`opacity: 0,1; x: ${isEven(index) ? '-100,0' : '100,0'}; end: 75vh + 50%;`}>
+      <div className={styles.profilePic} uk-parallax={`opacity: 0,1; y: 50,0; end: 85vh + 50%;`}>
       <img src={getImageUrl(data.attributes.profilePic)} />
       </div>
       <div className={styles.descWrapper}>
-        <div className={styles.descriptionLarge} uk-parallax={`opacity: 0,1; x: ${isEven(index) ? '100,0' : '0,100'}; y: 50,0; end: 75vh + 50%;`}>
+        <div className={styles.descriptionLarge} uk-parallax={`opacity: 0,1; y: 50,0; end: 85vh + 50%;`}>
           {
             data.attributes.guest && <h5>Gasttättowierer</h5>
           }
           <h3>{data.attributes.name}</h3>
           {data.attributes.description}
         </div>
-        <div className={styles.descriptionSmall} uk-parallax={`opacity: 0,1; x: ${isEven(index) ? '100,0' : '-100,0'}; y: 50,0; end: 75vh + 50%;`}>
+        <div className={styles.descriptionSmall} uk-parallax={`opacity: 0,1; y: 50,0; end: 85vh + 50%;`}>
           <div className={styles.artistHeader}>
             <div className={styles.artistLeft}>
               <h3>{data.attributes.name}</h3>
