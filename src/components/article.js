1c9efbd164269d686c4ceb539b35ef4343251133
jul <louis.jungo@ti8m.ch>
2022-11-09 18:22:10 +0100

    img fixes

--- src/components/article.js
+++ src/components/article.js
@@ -1,11 +1,13 @@
 import styles from '@/styles/Article.module.scss'
 
 export default function Article({ data }) {
+  console.log('LJ - data', data.attributes.image.data.attributes.formats);
+  
   return (
     <div className={styles.article}>
       <img
         className={styles.articleImage}
-        src={data?.attributes?.image?.data?.formats?.medium?.url || data?.attributes?.image?.data?.attributes?.url || ''}
+        src={data?.attributes?.image?.data?.attributes?.formats?.medium?.url || data?.attributes?.image?.data?.attributes?.url || ''}
         uk-parallax="opacity: 0,1; y: 50,0; end: 75vh + 50%"
       />
       <div className={styles.text} uk-parallax="opacity: 0,1; y: 50,0; end: 75vh + 50%">
