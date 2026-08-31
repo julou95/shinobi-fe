894982387beb6aec200bc1600af18b20b91bf5d6
julou95 <louis.jungo@gmail.com>
2026-08-31 22:38:11 +0200

    fixing fetch

--- src/api/strapi.js
+++ src/api/strapi.js
@@ -8,7 +8,7 @@ const headers = {
 }
 
 export const fetchContent = (path, sort = '') => {
-  return axios.get(`${strapiUrl()}/api/${path}?populate=*${sort}`, { headers })
+  return axios.get(`${strapiUrl()}api/${path}?populate=*${sort}`, { headers })
     .then(res => {
       return res.data.data
     })
